38f5c7c965ad9dc444ceb2f8a4bbe8ea02291ed6
ayushi <ayushi>
2023-09-18 23:31:45 +0530

    creation

---
+++ Linked  List/Double linked list/creation.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct node {
+int data;
+struct node *next;
+struct node *prev;
+}*first=NULL;
+
+void create(int A[],int n)
+{
+    int i;
+    struct node *last,*t;
+    first=(struct node *)malloc(sizeof(struct node));
+    first->data=A[0];
+    first->prev=first->next=NULL;
+    last=t;
+
+    for(i=1;i<n;i++)
+    {
+        t=(struct node *)malloc(sizeof(struct node));
+        t->data=A[i];
+        t->next=last->next;
+        t->prev=last;
+        last->next=t;
+        last=t;
+
+    }
+}
+void display(struct node *p)
+{
+    while(p!=NULL)
+    {
+        printf("%d",p->data);
+        p=p->next;
+    }
+}
+
+int main()
+{
+    int A[]={1,2,3,4,5};
+    create(A,5);
+    display(first);
+    return 0;
+}
