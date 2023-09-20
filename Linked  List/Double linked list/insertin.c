7284c6a5e405ec49d7ddca6b31097c2296d8cb06
Param <ps8425@srmist.edu.in>
2023-09-20 14:27:49 +0530

    commit

---
+++ Linked  List/Double linked list/insertin.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *prev;
+    struct node *next;
+}*first=NULL;
+
+void create(int A[],int n)
+{
+    int i;
+    struct node *last,*t;
+    first=(struct node *)malloc(sizeof(struct node));
+    first->data=A[0];
+    first->prev=first->next=NULL;
+    last=first;
+
+    for(i=1;i<n;i++)
+    {
+        t=(struct node *)malloc(sizeof(struct node));
+        t->data=A[i];
+        t->next=last->next;
+        t->prev=last;
+        last->next=t;
+        last=t;
+    }
+}
+int length(struct node *p)
+{
+    int l=0;
+    while(p!=0)
+    {
+       l++;
+       p=p->next;
+    }
+    return l;
+}
+
+void insert(struct node *p,int index,int x)
+{
+    struct node *t;
+    int i;
+    if(index<0||index>length(p))
+    {
+        return ;
+    }
+
+    if(index==0)
+    {
+        t=(struct node *)malloc(sizeof(struct node));
+        t->data=x;
+        t->prev=NULL;
+        t->next=first;
+        first->prev=t;
+        first=t;
+
+    }
+
+    else{
+      for(i=0;i<index-1;i++)
+      {
+        p=p->next;
+      }
+      t=(struct node *)malloc(sizeof(struct node));
+      t->data=x;
+      
+
+    }
+}
+
+void display(struct node *p)
+{
+    while(p!=0)
+    {
+        printf("%d",p->data);
+        p=p->next;
+    }
+}
+
+
+int main()
+{
+    int A[]={1,2,3,4,5};
+    create(A,5);
+    display(first);
+    return 0;
+}
