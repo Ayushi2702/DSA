90337a980457e1077c4e22d6307c6edf2ee645c6
Ayushi <ayushikarakoti02@gmail.com>
2023-10-18 11:13:32 +0530

    Commit

--- Arrays/Increasing array size.c
+++ Arrays/Increasing array size.c
@@ -3,7 +3,7 @@
 // The reason behind this limitation is related to how arrays are stored in computer memory.
 // When you create an array, the programming language allocates a contiguous block of memory to store the elements of the array.
 //  Each element in the array occupies a specific amount of memory, and they are stored one after another in this block of memory.
-//   The size of this block of memory is determined by the number of elements in the array multiplied by the size of each element.
+//  The size of this block of memory is determined by the number of elements in the array multiplied by the size of each element.
 // If you were allowed to dynamically change the size of an array (by increasing or decreasing it), it would require moving the entire array to a new location in the computer's memory.
 // This operation can be very time-consuming, especially for large arrays, because it would involve allocating a new block of memory, copying the existing elements to the new location, and then deallocating the old block of memory. This process is not efficient and can lead to performance issues, especially in time-sensitive applications
 
