fb8ceface64be4e7eed2ec90a3a43c9dd6591a5b
Ayushi <ayushikarakoti02@gmail.com>
2024-01-20 14:52:13 +0530

    commit

--- Stack/Stack_using_lists.c
+++ Stack/Stack_using_lists.c
@@ -42,3 +42,24 @@ int pop()
 }
 
 
+void display()
+{
+    struct Node *p;
+    p=top;
+    while(p!=NULL)
+    {
+        printf("%d ",p->data);
+        p=p->next;
+    }
+    printf("\n");
+}
+
+
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    display();
+}
